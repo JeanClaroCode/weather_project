31e49d6a7f84cd45423c12a3529fb0ca36ca7041
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-06-10 23:07:40 -0300

    Escondendo API

--- src/weather_service.js
+++ src/weather_service.js
@@ -1,19 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export async function getWeatherNeo(city) {
-    const apiKey = `2fe1d5e932ab5fc807517b74d82d5189`
-    const cityName = city
-    const celsiusMetric = "units=metric" 
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&${celsiusMetric}&lang=pt_br`
-    try { 
-        const response = await fetch(URL)
-        if(!response.ok) {
-            throw new Error('Network response was not ok')
+    const apiKey = process.env.API_KEY;
+    const cityName = city;
+    const celsiusMetric = "units=metric";
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&${celsiusMetric}&lang=pt_br`;
+
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
         const weatherData = await response.json();
-        return weatherData
-    } catch(error){
-        console.log(`Fetch erro: ${error}`)
-        throw error
+        return weatherData;
+    } catch (error) {
+        console.log(`Fetch erro: ${error}`);
+        throw error;
     }
-
 }
-
